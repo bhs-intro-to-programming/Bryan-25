f7b32f7b4d464f1689ab37a19b9d39b9c35ad6dd
Bryan-25 <111394909+Bryan-25@users.noreply.github.com>
2022-08-19 15:14:55 -0700

    Updating assignments/simple-draw/code.js

--- assignments/simple-draw/code.js
+++ assignments/simple-draw/code.js
@@ -38,7 +38,7 @@ drawLine(0, 600, 500, 0, 'red', 20)
 
 drawLine(0, 600, 0, 0, 'coral', 1000)
 
-drawLine(1000, -500, 0, 0, 'royalblue', 600)
+drawLine(1000, -400, 0, 0, 'royalblue', 600)
 
 drawLine(0, 600, 0, 0, 'black', 20)
 
