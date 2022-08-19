b11a5f143d376da46a36832b9b41623bbd0fe66f
Bryan-25 <111394909+Bryan-25@users.noreply.github.com>
2022-08-19 15:04:17 -0700

    Updating assignments/simple-draw/code.js

--- assignments/simple-draw/code.js
+++ assignments/simple-draw/code.js
@@ -52,6 +52,6 @@ drawRect(50, 50, 60, 40, 'black');
 
 drawFilledCircle( 250, 300, 50, 'yellow');
 
-drawFilledCircle( 250, 600, 300, 'blue');
+drawFilledCircle( 250, 600, 300, 'yellow');
 
 drawFilledRect(5, height - 20, 45, 15, 'purple');
