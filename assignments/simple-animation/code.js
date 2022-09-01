b7440c4ce53c8d08cc42d5f90214b4a9045fcc88
Bryan-25 <111394909+Bryan-25@users.noreply.github.com>
2022-09-01 15:05:28 -0700

    Updating assignments/simple-animation/code.js

--- assignments/simple-animation/code.js
+++ assignments/simple-animation/code.js
@@ -15,7 +15,7 @@ const drawFrame = (time) => {
 // model to draw other shapes.
 const drawTriangle = (x1, y1, x2, y2, x3, y3, color, width = 1) => {
   drawLine(x1, y1, x2, y2, color, width);
-  drawLine(x2, y2, x3, y3, color, width);
+  drawLine(x2, y2, x3, y3, color, width)
   drawLine(x3, y3, x1, y1, color, width);
 };
 
