cb69098fb6b758afb4a431a61a81f0809ad89303
Bryan-25 <111394909+Bryan-25@users.noreply.github.com>
2022-09-06 15:17:16 -0700

    Updating assignments/number-functions/code.js

--- assignments/number-functions/code.js
+++ assignments/number-functions/code.js
@@ -22,3 +22,6 @@ const averageOfThree = (a,b,c) => {
 const distance = (a,b) => {
   return Math.abs(a-b) ;
 };
+const manhattenDistance = (x1,x2,y1,y2) => {
+  return Math.abs(x1-x2) + Math.abs(y1-y2) ;
+};
