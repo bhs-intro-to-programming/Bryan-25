178bec4e3bbd5816e7d51e53e4d16800f9b25b8f
Bryan-25 <111394909+Bryan-25@users.noreply.github.com>
2022-09-06 15:12:21 -0700

    Updating assignments/number-functions/code.js

---
+++ assignments/number-functions/code.js
@@ -0,0 +1,18 @@
+const add = (a,b) => {
+  return a + b ;
+};
+const subtract = (a,b) => {
+  return a - b ;
+};
+const multiply = (a,b) => {
+  return a * b ;
+};
+const divide = (a,b) => {
+  return a / b ;
+};
+const mod = (a,b) => {
+  return a % b ;
+};
+const averageOfTwo = (a,b) => {
+  return (a+b)/2 ;
+};
