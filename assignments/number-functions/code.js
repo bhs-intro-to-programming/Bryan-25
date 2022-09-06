2d96d45087d58f18f99fdfccf40d07d9ff507b72
Bryan-25 <111394909+Bryan-25@users.noreply.github.com>
2022-09-06 15:26:19 -0700

    Updating assignments/number-functions/code.js

--- assignments/number-functions/code.js
+++ assignments/number-functions/code.js
@@ -25,3 +25,6 @@ const distance = (a,b) => {
 const manhattanDistance = (x1,x2,y1,y2) => {
   return Math.abs(x1-x2)+Math.abs(y1-y2) ;
 };
+const euclideanDistance = (x1,x2,y1,y2) => {
+  return Math.sqrt(Math.abs(x1-x2) * Math.abs(x1-x2) + Math.abs(y1-y2) * Math.abs(y1-y2)) ;
+};
