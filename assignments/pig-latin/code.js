7670c74c874f9cc61e3dd81ea46838db6fc0bddc
Bryan-25 <111394909+Bryan-25@users.noreply.github.com>
2022-09-12 15:26:34 -0700

    Updating assignments/pig-latin/code.js

--- assignments/pig-latin/code.js
+++ assignments/pig-latin/code.js
@@ -37,6 +37,6 @@ _fP.substring(_fP.search(/[aeiou]/),_fP.length)+
 _fP.substring(0,_fP.search(/[aeiou]/))+'ay'
 
 const advancedPigLatin = (_fP) =>
-_fP.search(/[aeiou]/ === 0 ?  _fP+'way': 
+_fP.search(/[aeiou]/) === 0 ?  _fP+'way': 
 _fP.substring(_fP.search(/[aeiou]/),_fP.length)+
 _fP.substring(0,_fP.search(/[aeiou]/))+'ay'
