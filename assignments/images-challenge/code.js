b4795a40d7690a6826304869b04c3e2cbcc9ca29
Bryan-25 <111394909+Bryan-25@users.noreply.github.com>
2022-09-21 15:05:02 -0700

    Updating assignments/images-challenge/code.js

--- assignments/images-challenge/code.js
+++ assignments/images-challenge/code.js
@@ -23,3 +23,15 @@
  *
  *  clear()
  */
+let radius = 23
+const lineOfCircles = (radius) => {
+  const diameter = 2*radius
+  const dist = Math.floor(width/diameter)*diameter
+  const extra = width-dist
+  let x = 0
+  while (x < width-extra){ 
+    drawFilledCircle(x+radius+extra/2, height/2, radius, 'red')
+    x += diameter
+  }
+}
+lineOfCircles (radius);
