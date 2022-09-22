7359fef9641885bb5a73c0af2d357b7f2c82d44a
Bryan-25 <111394909+Bryan-25@users.noreply.github.com>
2022-09-22 15:24:30 -0700

    Updating assignments/images-challenge/code.js

--- assignments/images-challenge/code.js
+++ assignments/images-challenge/code.js
@@ -47,6 +47,7 @@ while (x < width - extra) {
   drawFilledCircle(x + radius + extra / 2, height / 2, radius, color)
   x += diameter
   roy = roy + 1
-  if roy %2 == 0
+  if (roy %2 == 0)
+  if (roy %2 == 1)
 }
 }
