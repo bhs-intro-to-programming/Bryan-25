440ce93dd2b2d13e66195050a0f0ee1b5654aa7c
Bryan-25 <111394909+Bryan-25@users.noreply.github.com>
2022-09-29 14:42:45 -0700

    Updating assignments/images-challenge/code.js

--- assignments/images-challenge/code.js
+++ assignments/images-challenge/code.js
@@ -67,7 +67,7 @@ const dartBoard = (score) => {
       color = 'red';
     }
     roy = roy + 1
-    factor = height / 2 - ((height / 2) / score * c)
+    factor = height / 2 - ((height / 2) / score * i)
   }
 
 }
