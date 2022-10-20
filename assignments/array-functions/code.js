516fabb9f472f03124049b3c585267048aa8b901
Bryan-25 <111394909+Bryan-25@users.noreply.github.com>
2022-10-20 15:03:39 -0700

    Updating assignments/array-functions/code.js

--- assignments/array-functions/code.js
+++ assignments/array-functions/code.js
@@ -2,7 +2,7 @@ const countTens = (numeros) => {
   let ten = 0
   for (let i = 0; i < numeros.length + 1; i++) {
     if (numeros[i] == 10) {
-      ten + 1
+      ten = ten + 1
     }
   };
   return;
