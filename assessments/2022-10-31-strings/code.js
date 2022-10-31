2c0da0d02a9f2e0771ae21fb7c80885be3088330
Bryan-25 <111394909+Bryan-25@users.noreply.github.com>
2022-10-31 15:15:56 -0700

    Updating assessments/2022-10-31-strings/code.js

--- assessments/2022-10-31-strings/code.js
+++ assessments/2022-10-31-strings/code.js
@@ -30,3 +30,7 @@ const firstFewEveryOther = (s) => {
 const upDownLastCharacter = (s) => {
   return s.toUpperCase(s.substring(s.length-1,s.length)) + s.toLowerCase(s.substring(s.length-1,s.length));
 };
+
+const firstAndLast = (s) => {
+  return s.substring(0) + s.substring(s.length);
+};
