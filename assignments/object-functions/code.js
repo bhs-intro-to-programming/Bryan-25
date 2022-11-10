26c77fdf9ed2653b28ab8ddf2825fcde95a86723
Bryan-25 <111394909+Bryan-25@users.noreply.github.com>
2022-11-10 15:30:33 -0800

    Updating assignments/object-functions/code.js

--- assignments/object-functions/code.js
+++ assignments/object-functions/code.js
@@ -11,5 +11,5 @@ const distance = (p1,p2) => {
   return Math.sqrt(( p1['x'] - p2['x']) ** 2 +  (p1['y'] - p2['y'])  ** 2)
 }
 const midpoint = (p1,p2) => {
-  return Math.sqrt( (p1['x']/2) - (p2['x']/2)) +  ((p1['y']/2) - (p2['y']/2))
+  return Math.sqrt((p1['x']/2) - (p2['x']/2)) +  ((p1['y']/2) - (p2['y']/2))
 }
