239b7541a68070676459604e5f4d363267aaab07
Bryan-25 <111394909+Bryan-25@users.noreply.github.com>
2022-11-10 15:13:07 -0800

    Updating assignments/object-functions/code.js

--- assignments/object-functions/code.js
+++ assignments/object-functions/code.js
@@ -7,6 +7,6 @@ return { 'x': object1, 'y': object2 }
  const emptyObject = () => {
   return {}
 }
- const distance = () => {
-  return Math.sqrt{ 'x': object1, 'y': object2 }
+ const distance = (object1, object2) => {
+  return Math.sqrt{ 'x': object1 + 'y': object2 } { 'x': object1 + 'y': object2 }
 }
