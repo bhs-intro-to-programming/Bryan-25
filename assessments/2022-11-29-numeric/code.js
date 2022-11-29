c309ce292478e079b101ab12352c98e47bcc86f0
Bryan-25 <111394909+Bryan-25@users.noreply.github.com>
2022-11-29 15:18:01 -0800

    Updating assessments/2022-11-29-numeric/code.js

--- assessments/2022-11-29-numeric/code.js
+++ assessments/2022-11-29-numeric/code.js
@@ -26,5 +26,5 @@ const wrappingCombos = (numberK,numberR,numberB) => {
   return numberK*numberR*numberB
 };
 const biggestNumber = (digits) => {
-  return 9*10**digits
+  return 9*10**(digits-1)
 };
