9f3dfc29e063c9d5968de5e2bf27387b6b648fdb
Bryan-25 <111394909+Bryan-25@users.noreply.github.com>
2022-11-29 15:31:15 -0800

    Updating assessments/2022-11-29-numeric/code.js

--- assessments/2022-11-29-numeric/code.js
+++ assessments/2022-11-29-numeric/code.js
@@ -1,32 +1,32 @@
 const totalEggs = (hEgg, sEgg) => {
   return hEgg + sEgg
 };
-const chocolatesPerPerson = (c,p) => {
-  return Math.floor(c/p)
+const chocolatesPerPerson = (c, p) => {
+  return Math.floor(c / p)
 };
-const extraChocolates = (c,p) => {
-  return c%p
+const extraChocolates = (c, p) => {
+  return c % p
 };
-const leftOut = (c,p) => {
-  if p-c == %2
-  return p-c
+const leftOut = (c, p) => { /*????????*/
+  if p - c == <1 
+  return p - c
   else return 0
 };
 const probabilityAllHeads = (flips) => {
-  return 0.5**flips
+  return 0.5 ** flips
 };
 const futureHour = (current, future) => {
   return current + future
 };
-const presentsBudget = (friends,price) => {
-  return friends*price
+const presentsBudget = (friends, price) => {
+  return friends * price
 };
 const perPresent = (budget, presents) => {
-  return budget/presents
+  return budget / presents
 };
-const wrappingCombos = (numberK,numberR,numberB) => {
-  return numberK*numberR*numberB
+const wrappingCombos = (numberK, numberR, numberB) => {
+  return numberK * numberR * numberB
 };
 const biggestNumber = (digits) => {
-  return 
+  return
 };
