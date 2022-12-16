f483e295723d52da3010f8006926bf079327e8d6
Bryan-25 <111394909+Bryan-25@users.noreply.github.com>
2022-12-16 11:58:08 -0800

    Updating assessments/2022-12-16-fall-semester-final/code.js

--- assessments/2022-12-16-fall-semester-final/code.js
+++ assessments/2022-12-16-fall-semester-final/code.js
@@ -152,9 +152,13 @@ const isAllUpperCase = (s) => {
 }
 
 const sameIgnoringCase = (s1, s2) => {
-  if (s1.toUpperCase() === s2.toUpperCase())  {
+  if (s1.toUpperCase() === s2.toUpperCase()) {
     return true
   } else {
     return false
   }
+}
+
+const firstName = (s) => {
+  return s.substring(0, indexOf[' '])
 }
